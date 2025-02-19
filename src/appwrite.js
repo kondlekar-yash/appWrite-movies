597564765fab089ce97ed649eefc80676b4dab95
kondlekaryash <yashk9100@gmail.com>
2025-02-19 17:32:53 +1100

    keys reverted

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,8 +1,8 @@
 import { Client, Databases, ID, Query } from "appwrite";
 
-const PROJECT_ID = import.meta.env.APPWRITE_PROJECT_ID;
-const DATABASE_ID = import.meta.env.APPWRITE_DATABASE_ID;
-const COLLECTION_ID = import.meta.env.APPWRITE_COLLECTION_ID;
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1")
